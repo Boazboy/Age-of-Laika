5d66bd03c962b5f6018f94b725e3ebe42238f552
Boazboy <BoazboyIII@gmail.com>
2016-01-18 19:43:19 -0500

    Testing

--- Age of Laika/ViewController.swift
+++ Age of Laika/ViewController.swift
@@ -9,16 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+   
+    @IBOutlet weak var convertedTo: UITextField!
+    
+    @IBOutlet weak var howOldIsDogLabel: UILabel!
+    
+    @IBOutlet weak var howOldDogIsTextField: UITextField!
+    
+    @IBOutlet weak var convertedDogYears: UIButton!
+        
+    @IBAction func HoldOldDogIs(sender: AnyObject) {
+        
+        
+        
+        var convertedDogYearsint = howOldDogIsTextField.text.toInt()
+        
+        //var convertedDogYearsint = 0
+        
+        //var convertedDogYears = 0
+        
+        var convertedDogYears = convertedDogYearsint! * 7
+        
+        
+        
+        convertedTo.text = "Your Dog is years old in dog years."
+        
+                                                    }
+    
+        override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
+                                    }
+        
+   
+        override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
+                                                }
 
 
 }
